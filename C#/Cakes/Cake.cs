d16343d0eb7c59d1c0e5922c839ebdd7cb4dad75
xelond-dev <142461429+xelond-dev@users.noreply.github.com>
2023-10-26 00:18:46 +0300

    Added cakes (pre-finished).

---
+++ C#/Cakes/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cakes
+{
+    internal class Cake
+    {
+        public string form = "Круглый";
+        public string size = "Средний";
+        public string taste = "Клубничный";
+        public string glaze = "Кремовая";
+        public string decor = "Звездочки";
+        public int count = 1;
+    }
+}
---
+++ C#/Cakes/Menu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cakes
+{
+    internal class Menu
+    {
+        private int maxPosition;
+        public Menu(int max)
+        {
+            maxPosition = max;
+        }
+
+        public int Show()
+        {
+            int position = 7;
+
+            while (true)
+            {
+                Console.SetCursorPosition(1, position);
+                Console.WriteLine("*");
+
+                ConsoleKeyInfo KeyReader = Console.ReadKey();
+
+                switch (KeyReader.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.SetCursorPosition(1, position);
+                        Console.WriteLine(" ");
+                        position--;
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        Console.SetCursorPosition(1, position);
+                        Console.WriteLine(" ");
+                        position++;
+                        break;
+
+                    case ConsoleKey.Enter:
+
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
