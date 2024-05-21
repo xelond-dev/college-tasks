fb91f2c0f1bc07a58ea6433e31063e98353c0046
xelond-dev <xelond@proton.me>
2024-05-21 15:15:54 +0300

    Socketiiii vauuuu

---
+++ socket-master/SocketClient/SocketClient/SocketClient.cpp
@@ -0,0 +1,103 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+int main() {
+    WSADATA wsaData;
+    ADDRINFO hints;
+    ADDRINFO* addrResult = nullptr;
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    char recvBuffer[512];
+
+    const char* sendBuffer1 = "Hello from client 1";
+    const char* sendBuffer2 = "Hello from client 2";
+
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) {
+        cout << "WSAStartup failed with result: " << result << endl;
+        return 1;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    result = getaddrinfo("localhost", "666", &hints, &addrResult);
+    if (result != 0) {
+        cout << "getaddrinfo failed with error: " << result << endl;
+        WSACleanup();
+        return 1;
+    }
+
+    ConnectSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+    if (ConnectSocket == INVALID_SOCKET) {
+        cout << "Socket creation failed with error: " << WSAGetLastError() << endl;
+        freeaddrinfo(addrResult);
+        WSACleanup();
+        return 1;
+    }
+
+    result = connect(ConnectSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen);
+    if (result == SOCKET_ERROR) {
+        cout << "Unable to connect to server with error: " << WSAGetLastError() << endl;
+        closesocket(ConnectSocket);
+        freeaddrinfo(addrResult);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(addrResult);
+
+    result = send(ConnectSocket, sendBuffer1, (int)strlen(sendBuffer1), 0);
+    if (result == SOCKET_ERROR) {
+        cout << "Send failed with error: " << WSAGetLastError() << endl;
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+    cout << "Sent: " << result << " bytes" << endl;
+
+    result = send(ConnectSocket, sendBuffer2, (int)strlen(sendBuffer2), 0);
+    if (result == SOCKET_ERROR) {
+        cout << "Send failed with error: " << WSAGetLastError() << endl;
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+    cout << "Sent: " << result << " bytes" << endl;
+
+    result = shutdown(ConnectSocket, SD_SEND);
+    if (result == SOCKET_ERROR) {
+        cout << "Shutdown failed with error: " << WSAGetLastError() << endl;
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    do {
+        ZeroMemory(recvBuffer, 512);
+        result = recv(ConnectSocket, recvBuffer, 512, 0);
+        if (result > 0) {
+            cout << "Received " << result << " bytes" << endl;
+            cout << "Received data: " << recvBuffer << endl;
+        }
+        else if (result == 0) {
+            cout << "Connection closed" << endl;
+        }
+        else {
+            cout << "Recv failed with error: " << WSAGetLastError() << endl;
+        }
+    } while (result > 0);
+
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 0;
+}
