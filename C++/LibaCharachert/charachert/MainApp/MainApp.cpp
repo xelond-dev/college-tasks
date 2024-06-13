cbfea405871f886c133a5fb043f534c24154f210
xelond-dev <xelond@proton.me>
2024-06-13 19:23:14 +0300

    libat

---
+++ C++/LibaCharachert/charachert/MainApp/MainApp.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <conio.h>
+
+typedef bool(__stdcall* CheckCharactersFunc)(const char*, const char*);
+
+int main() {
+    // Запрос строки и символов у пользователя
+    std::string inputString, characters;
+    std::cout << "Write bro: ";
+    std::getline(std::cin, inputString);
+    std::cout << "Enter to find: ";
+    std::getline(std::cin, characters);
+
+    // Проверка ввода
+    std::cout << "Input string: " << inputString << std::endl;
+    std::cout << "Characters to find: " << characters << std::endl;
+
+    // Загрузка библиотеки
+    HMODULE hLib = LoadLibrary(TEXT("StringChecker.dll"));
+    if (hLib == NULL) {
+        std::cerr << "Sorry :( Could not load the DLL." << std::endl;
+        return 1;
+    }
+    std::cout << "DLL loaded successfully." << std::endl;
+
+    // Получение адреса функции
+    CheckCharactersFunc checkCharacters = (CheckCharactersFunc)GetProcAddress(hLib, "CheckCharacters");
+    if (!checkCharacters) {
+        std::cerr << "Sorry :( Could not find the function" << std::endl;
+        FreeLibrary(hLib);
+        return 1;
+    }
+    std::cout << "Function address retrieved successfully." << std::endl;
+
+    // Вызов функции
+    bool result = checkCharacters(inputString.c_str(), characters.c_str());
+
+    // Вывод результата
+    if (result) {
+        std::cout << "All characters was find." << std::endl;
+    }
+    else {
+        std::cout << "Sorry :( Charachters is not founded." << std::endl;
+    }
+
+    FreeLibrary(hLib);
+
+    while (true) {
+        if (_kbhit()) {
+            int ch = _getch();
+            if (ch == 3) {
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ C++/LibaCharachert/charachert/StringChecker/StringChecker.h
@@ -0,0 +1,22 @@
+﻿// Приведенный ниже блок ifdef — это стандартный метод создания макросов, упрощающий процедуру
+// экспорта из библиотек DLL. Все файлы данной DLL скомпилированы с использованием символа STRINGCHECKER_EXPORTS
+// Символ, определенный в командной строке. Этот символ не должен быть определен в каком-либо проекте,
+// использующем данную DLL. Благодаря этому любой другой проект, исходные файлы которого включают данный файл, видит
+// функции STRINGCHECKER_API как импортированные из DLL, тогда как данная DLL видит символы,
+// определяемые данным макросом, как экспортированные.
+#ifdef STRINGCHECKER_EXPORTS
+#define STRINGCHECKER_API __declspec(dllexport)
+#else
+#define STRINGCHECKER_API __declspec(dllimport)
+#endif
+
+// Этот класс экспортирован из библиотеки DLL
+class STRINGCHECKER_API CStringChecker {
+public:
+	CStringChecker(void);
+	// TODO: добавьте сюда свои методы.
+};
+
+extern STRINGCHECKER_API int nStringChecker;
+
+STRINGCHECKER_API int fnStringChecker(void);
